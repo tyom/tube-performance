9d12721f9554484b5ff17a95f6d2adbbfe3ca722
Tyom Semonov <inspiro@gmail.com>
2017-11-22 14:06:58 +0000

    Generate reporting period dates

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 const XLSX = require('xlsx')
-const { assign } = require('lodash')
-
-const { REPORTING_PERIODS } = require('./constants')
+const { endOfWeek, addWeeks, addDays } = require('date-fns')
 
 const workbook = XLSX.readFile('./data/lu-performance-data-almanac.xlsx')
 const currentSheet = workbook.Sheets['Platform Wait Time']
+
+const TOTAL_PERIOD_COUNT = 13
 
 function getPeriodValues (array) {
   return array
@@ -24,17 +24,38 @@ function transformSheetToJson (sheet) {
     const heading = current[0]
     const isPeriodHeading = heading === 'Platform Wait Time'
     const isTotal = heading === 'TOTAL ALL LINES'
+    let currentYear = parseInt(currentPeriod && currentPeriod.split('/')[0])
+    const firstPeriodDate = new Date(`${currentYear}-04-01`)
+    const lastPeriodDate = new Date(`${currentYear + 1}-03-31`)
 
     if (isPeriodHeading) {
       currentPeriod = current[1]
       result[currentPeriod] = {
-        dates: REPORTING_PERIODS[currentPeriod],
         lines: {},
       }
-    } else if (isTotal) {
-      result[currentPeriod].linesTotal = getPeriodValues(current)
+      return result
+    }
+
+    const currentPeriodValues = getPeriodValues(current)
+
+    if (isTotal) {
+      result[currentPeriod].periodDates = currentPeriodValues.map((value, i) => {
+        // LU has 13 reporting periods of 28 days, from April to March.
+        // The lengths of periods 1 and 13 may however vary to align with the financial year end of 31 March
+        let startOfPeriodDate = addWeeks(endOfWeek(firstPeriodDate), 4 * i)
+        let endOfPeriodDate = addWeeks(startOfPeriodDate, 4)
+
+        startOfPeriodDate = i === 0 ? firstPeriodDate : addDays(startOfPeriodDate, 1)
+        endOfPeriodDate = i === TOTAL_PERIOD_COUNT - 1 ? lastPeriodDate : endOfPeriodDate
+
+        return {
+          from: startOfPeriodDate,
+          to: endOfPeriodDate,
+        }
+      })
+      result[currentPeriod].linesTotal = currentPeriodValues
     } else {
-      result[currentPeriod].lines[heading] = getPeriodValues(current)
+      result[currentPeriod].lines[heading] = currentPeriodValues
     }
 
     return result
@@ -44,11 +65,6 @@ function transformSheetToJson (sheet) {
     name: sheetArray[0][0],
     periods: transformedPeriods,
   }
-
-  // console.dir(
-  //   result,
-  //   { depth: null },
-  // )
 
   return JSON.stringify(result, null, 2)
 }
