884b18c1c66071c3d7fb7e6eed3e0ab3f9333f2b
Tyom Semonov <inspiro@gmail.com>
2017-11-24 16:11:10 +0000

    Build JSON files automatically from sheets

--- scripts/build-json-from-xlsx.js
+++ scripts/build-json-from-xlsx.js
@@ -1,3 +1,7 @@
 const { buildJsonFromSheet } = require('../src/transform')
+const { loadSpreadsheet, saveToJson } = require('../src/transform')
 
-buildJsonFromSheet('Platform Wait Time')
+const workbook = loadSpreadsheet()
+const sheetNames = workbook.SheetNames.slice(23, 27)
+
+sheetNames.forEach(buildJsonFromSheet)
