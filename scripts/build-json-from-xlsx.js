bd303b9eed78f87d5f0b7552e3364fa1a0aa8700
Tyom Semonov <inspiro@gmail.com>
2017-11-27 12:12:54 +0000

    Include more sheets

--- scripts/build-json-from-xlsx.js
+++ scripts/build-json-from-xlsx.js
@@ -2,6 +2,6 @@ const path = require('path')
 const { loadSpreadsheet, buildJsonFromWorkbookSheets } = require('../src/transform')
 
 const workbook = loadSpreadsheet(path.join(__dirname, '../data/lu-performance-data-almanac.xlsx'))
-const sheetNames = workbook.SheetNames.slice(23, 27)
+const sheetNames = workbook.SheetNames.slice(22, 30)
 
 buildJsonFromWorkbookSheets(workbook, sheetNames)
